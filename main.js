22de501bcfd640ea1a5354d90efe756e34c3c775
Eric <eric.vercheval@hotmail.fr>
2021-04-06 08:27:40 +0200

    control form with dataset

--- main.js
+++ main.js
@@ -1,39 +1,32 @@
-console.log('hello javascript')
+// console.log('hello javascript')
 
-const button = document.getElementById('form-button')
-
-/* creation style */
-var style = document.createElement('style');
-// style.innerHTML = `
-// #form-prenom::before {
-//     content: "pour teste";
-//     display: block;
-//     color: #F00;
-//     position: absolute;
-//     z-index: 10;
-//     top: 10px;
-//     left: 10px;
-// }
-// `
-// document.getElementsByTagName('head')[0].appendChild(style);
 
 /* selection des elements dans form sauf submit */
 const elements = document.querySelectorAll('form > *:not([type=submit])')
+
+/* parcours des elements */
 elements.forEach(element => {
+  /* redefinition css avant wrapping */
   element.style.position = "relative"
   element.style.width = "100%"
   element.style.height  = "100%"
-
+  
+  /* creation des span (wrapper) */
   const span = document.createElement('span')
   span.style.position = "relative"
   span.id = `span-${element.id}`
   span.style.gridArea = element.id.replace("form-", "")
-  // span.style.setProperty("--text-error", `0`)
-  
+  /* insertion dans DOM */
   element.parentNode.insertBefore(span, element)
+  /* placement element dans span */
   span.appendChild(element)
-  // element.appendChild(span)
+  /* event */
+  element.addEventListener('blur', () => {
+    validationElement(element)
+  })
 });
+
+/* creation style et insertion DOM (head) */
 var style = document.createElement('style');
 style.innerHTML = `
 span::before { 
@@ -49,108 +42,81 @@ span::before {
   display: block;
   background-color: transparent;
 }
+.form-error {
+  border: 1px dot red;
+}
 `;
 document.getElementsByTagName('head')[0].appendChild(style);
 
 
+/* selection du submit */
+const button = document.getElementById('form-button')
 /* détection de la validation du formulaire */
 button.addEventListener('click', e => {
   e.preventDefault()
 
   validationForm()
-  console.log('Bien tenté ;)');
 })
 
+/* validation du formulaire */
 function validationForm() {
   
-  /* les champs a verifier */
-  const elements = [
-    {
-      name: "prenom",
-      empty: true,
-      len:  4
-    }, 
-    {
-      name:"nom",
-      empty: true,
-      len: 5
-    }, 
-    {
-      name: "email",
-      empty: true,
-      email: true
-    },
-    {
-      name: "sujet",
-      empty: true,
-      len: 10
-    },
-    {
-      name: "text",
-      empty: true,
-      len: 40
-    }
-  ]
-
-  
-  function formVide(value) {
-
-    return (value.trim() == "")
-
-  }
-  
-  function setMessage(span, text) {
-
-  }
-
+  let errors = 0
   /* parcour des elements */
-  for ( const el of elements) {
-    const name = el.name
-    const $el = document.getElementById(`form-${name}`)
-    const $span = document.getElementById(`span-form-${name}`)
-    /* enlever les classes */
-    $el.classList.remove('error')
-    /* enlever les message */
-    $span.style.setProperty("--error-message", `\"\"`)
-
-    // recup de la valeur
-    let val = $el.value.trim()
+  for ( const $el of elements) {
     
-    /* verif si vide ? */
-    if (el.empty && val == "") {
-      /* cadre pour erreur */
-      $el.classList.add('error')
-      /* message d'erreur */
-      $span.style.setProperty("--error-message", `\"Doit pas être vide.\"`)
-      continue
-    }
-    
-    /* verif longueur */
-    if(el.len !== undefined && val.length < el.len) {
-      /* cadre pour erreur */
-      $el.classList.add('error')
-      /* message d'erreur */
-      $span.style.setProperty("--error-message",`\"Doit contenir ${el.len} caractères.\"`)
-      continue
-    }
-
-    /* verif email */
-    if(el.email) {
-      /* TODO : trouver le bon format pour email*/
-      const mailformat = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
-      if (!$el.value.match(mailformat)) {
-        /* cadre pour erreur */
-        $el.classList.add('error')
-        /* message d'erreur */
-        $span.style.setProperty("--error-message",`\"Doit contenir une adresse email valide.\"`)
-        continue
-      }
-    }
-
-
-    console.log(`${name} : ${val}`)
+    if (!validationElement($el)) errors++ 
     
   }
-
   
+  /* TODO : message si aucune erreur (errors=0)*/
+  console.log(`errors : ${errors}`);
+  console.log(`${errors == 0 ? `message envoyé` : `Bien tenté ;)`}`) 
+  
+}
+
+function validationElement($el) {
+  const $span = document.getElementById(`span-${$el.id}`)
+    
+  console.log(parseInt($el.dataset.length,10) )
+  console.log(parseInt($el.dataset.length, 10) ? true : false);
+  
+  /* enlever les classes */
+  $el.classList.remove('form-error')
+  /* enlever les message */
+  $span.style.setProperty("--error-message", `\"\"`)
+  
+  // recup de la valeur sans espaces
+  let val = $el.value.trim()
+  
+  
+  /* verif longueur */
+  const length = parseInt($el.dataset.length, 10)
+  if( length && parseInt( val.length, 10) < length) {
+    /* cadre pour erreur */
+    $el.classList.add('form-error')
+    /* message d'erreur */
+    $span.style.setProperty("--error-message",`\"Doit contenir ${length} caractères.\"`)
+    $el.setCustomValidity("erreur setCustomValidity")
+    return false
+  }
+
+  /* verif email */
+  const email = $el.dataset.email
+  if(email) {
+    /* TODO : trouver le bon format pour email*/
+    // const mailformat = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+    // https://www.regextester.com/19
+    // const mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+
+    const mailformat = /^[0-9a-z._-]+@{1}[0-9a-z.-]{2,}[.]{1}[a-z]{2,5}$/
+    if (!val.match(mailformat)) {
+      /* cadre pour erreur */
+      $el.classList.add('form-error')
+      /* message d'erreur qui ne fonctionne pas? */
+      $span.style.setProperty("--error-message",`\"Doit contenir une adresse email valide.\"`)
+      return false
+    }
+  }
+  return true
 }
