0aa6952e3c91c9958a130d461f07145721dee10e
Eric <eric.vercheval@hotmail.fr>
2021-06-07 09:14:11 +0200

    test implement post

--- exercices/we-transfer/public/index.php
+++ exercices/we-transfer/public/index.php
@@ -4,14 +4,38 @@ $loader = require '../vendor/autoload.php';
 
 use App\Model\MyMail;
 
-if (isset($_POST["message"])) {
-    echo "\$_POST : <br>";
-    dump($_POST);
-    die;
+function sendMail()
+{
     $myMail = new MyMail();
     dump($myMail);
 
     $myMail->sendEmail();
 }
 
-include "../src/View/IndexTemplate.php";
+if (!empty($_POST)) {
+    dump($_POST);
+
+    dump($_FILES);
+    // $files
+    $nbFiles = count($_FILES['files']['name']);
+    for ($i = 0; $i < $nbFiles; $i++) {
+        $files[$i] = [
+            'name' => $_FILES['files']['name'][$i],
+            'type' => $_FILES['files']['type'][$i],
+            'tmp_name' => $_FILES['files']['tmp_name'][$i],
+            'error' => $_FILES['files']['error'][$i],
+            'size' => $_FILES['files']['size'][$i],
+        ];
+    }
+
+
+
+    dump($files);
+    foreach ($_FILES['files'] as $key => $value) {
+
+        echo $key . '<br>'; //. "=>" . $value . "<br>";
+    }
+}
+if (empty($_POST)) {
+    include "../src/View/IndexTemplate.php";
+}
