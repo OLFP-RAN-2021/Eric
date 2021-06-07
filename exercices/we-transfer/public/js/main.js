acd4c0945e0950bf79c9317551db23653df99529
Eric <eric.vercheval@hotmail.fr>
2021-06-07 09:07:32 +0200

    drop & noscript

--- exercices/we-transfer/public/js/main.js
+++ exercices/we-transfer/public/js/main.js
@@ -1,16 +1,22 @@
 console.log('dans main.js')
 
 const btnSend = document.querySelector('#btnSend')
+const dropZone = document.querySelector('#drop-zone')
 const emailSender = document.querySelector('#emailSender')
 const emailDest = document.querySelector('#emailDest')
-const dropZone = document.querySelector('#drop-zone')
+const form = document.querySelector('form')
+const inputFiles = document.querySelector('#files')
+const fileList = inputFiles.files
+
+let files
+
+form.style.display = "flex"
 
 btnSend.addEventListener('click', envoiMessage)
 
-dropZone.addEventListener('drop', e => {
-    e.preventDefault()
-    console.log(e.target);
-})
+dropZone.addEventListener('drop', addDropFiles)
+// dropZone.addEventListener('change', addFiles)
+
 dropZone.addEventListener('dragover', e => {
     e.preventDefault()
     if (
@@ -25,8 +31,48 @@ dropZone.addEventListener('dragleave', e => {
     dropZone.classList.remove('valid', 'error')
 })
 
-function envoiMessage(e) {
-    e.preventDefault()
+// function addFiles(e) {
+//     console.log('dans addFiles')
+//     console.log(inputFiles.files)
 
-    console.log(emailSender, emailDest);
+//     debugger
+// }
+function addDropFiles(e) {
+    e.preventDefault()
+    files = e.dataTransfer.files
+    // console.table(files)
+    inputFiles.files = files
+    // console.log(inputFiles.files)
+    // console.log(fileList)
 }
+
+async function envoiMessage(e) {
+    e.preventDefault()
+    const myForm = document.getElementById('#myForm');
+    const formData = new FormData(myForm);
+    // formData.append('file', files)
+
+    console.log('formData : ', formData)
+    debugger
+    for (const pair of formData) {
+        console.log(pair[0], pair[1]);
+    }
+    // console.log(emailSender.value, emailDest.value);
+    let response = await fetch("reception.php", {
+        method: 'POST',
+        body: formData
+    })
+    if (response.ok) {
+        const data = await response.text()
+        console.log('reponse : ', data)
+    }
+    // .then(response => response.text())
+    // .then(data => console.log('reponse : ', data))
+    // .then(data => data)
+}
+
+/* couper les listeners de la page */
+let actions = ['dragover', 'drop']
+actions.forEach(action => {
+    document.body.addEventListener(action, e => e.preventDefault())
+})
